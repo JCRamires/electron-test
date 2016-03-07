28f4aac08f05b6a68dc3c3e80578b5862da2a529
Juan Carlos Ramires <jcramires851@gmail.com>
2016-03-06 22:01:13 -0300

    commit inicial

---
+++ js/index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const Datastore = require('nedb')
+const db = new Datastore({filename: 'database', autoload: true})
+
+const Index = (function(){
+
+    function init(){
+        binds()
+    }
+
+    function binds(){
+        $('#pessoa-form').on('submit', function(event) {
+            event.preventDefault()
+            let pessoaJSON = parsePessoaToJSON($(this).serializeArray())
+            addPessoa(pessoaJSON)
+        })
+    }
+
+    function parsePessoaToJSON(pessoa){
+        let pessoaJSON = {}
+        for (let attribute of pessoa){
+            pessoaJSON[attribute.name] = attribute.value
+        }
+
+        return pessoaJSON
+    }
+
+    function addPessoa(pessoa){
+        db.insert(pessoa)
+    }
+
+    return {
+        init: init
+    }
+})()
